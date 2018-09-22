d95a62ec751c714e63edb8f3a206b4a94c08e33f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-09-22 23:25:48 +0200

    [ADD] Add basic SQLAlchemy connection to SQLite DB

---
+++ frostmark/db_base.py
@@ -0,0 +1,19 @@
+'''
+Module for creating a connection to SQLite DB
+and SQLAlchemy declarative base home.
+'''
+
+from os.path import join, dirname, abspath
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from frostmark import user_data
+
+DB_NAME = 'user_db.sqlite'
+DB_PATH = join(dirname(abspath(user_data.__file__)), DB_NAME)
+ENGINE = create_engine(f'sqlite:///{DB_PATH}', echo=True)
+BASE = declarative_base()
+SESSIONMAKER = sessionmaker()
+SESSIONMAKER.configure(bind=ENGINE)
