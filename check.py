2089002d513afecb802ebb7fc7244ad2ff64613a
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-09-23 00:17:47 +0200

    [FIX] Change style check to general check running lint + tests

--- check.py
+++ check.py
@@ -6,10 +6,25 @@ from subprocess import Popen
 from os.path import join, dirname, abspath
 
 ROOT = dirname(abspath(__file__))
-CASES = [
-    ['pycodestyle', f'{ROOT}'],
-    ['pylint', 'check.py', 'setup.py', f'{join(ROOT, "frostmark")}']
-]
+PKG = join(ROOT, 'frostmark')
+CASES = [[
+    'pycodestyle',
+    '--ignore=none',
+    '--show-source',
+    '--count',
+    '--max-line-length=79',
+    f'{ROOT}'
+], [
+    'pylint',
+    '--jobs=0',
+    'check.py', 'setup.py', PKG
+], [
+    'python', '-m', 'unittest', 'discover',
+    '--failfast',
+    '--catch',
+    '--start-directory', join(PKG, 'tests'),
+    '--top-level-directory', PKG
+]]
 
 
 if __name__ == '__main__':
