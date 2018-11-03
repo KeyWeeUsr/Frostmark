30ca3fb95ff5cde16f9f60063f538a6eeac04025
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2018-11-03 01:37:07 +0100

    [ADD] Add Bookmark and Folder tables

--- frostmark/db.py
+++ frostmark/db.py
@@ -3,6 +3,24 @@ Module for creating SQLite DB schema and session retrieving.
 '''
 
 from frostmark.db_base import BASE, SESSIONMAKER, ENGINE
+from frostmark.models import Folder, Bookmark  # pylint: disable=unused-import
+
+
+def folder_check_root(session):
+    '''
+    Check whether default rows for Folder model exist and create them if not.
+    '''
+
+    folders = session.query(Folder).all()
+    root_folder = Folder.get_root()
+
+    if not any([fold.id == root_folder[0] for fold in folders]):
+        session.add(Folder(
+            id=root_folder[0],
+            name=root_folder[1],
+            parent_folder_id=root_folder[0]
+        ))
+    session.commit()
 
 
 def get_session():
@@ -11,4 +29,6 @@ def get_session():
     '''
 
     BASE.metadata.create_all(ENGINE)
-    return SESSIONMAKER()
+    session = SESSIONMAKER()
+    folder_check_root(session)
+    return session
---
+++ frostmark/models.py
@@ -0,0 +1,73 @@
+'''
+ORM SQLAlchemy models for Frostmark application storage.
+'''
+
+from sqlalchemy import Column, Integer, String, BLOB, ForeignKey
+from sqlalchemy.orm import relationship
+from frostmark.db import BASE
+
+
+class Folder(BASE):
+    '''
+    Folder item for grouping bookmarks.
+    ID 0 is reserved for ROOT folder.
+    '''
+    # pylint: disable=too-few-public-methods
+
+    __tablename__ = 'folder'
+
+    __ROOT_ID__ = 0
+    __ROOT_NAME__ = 'ROOT'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String, nullable=False, default='')
+    parent_folder_id = Column(Integer, nullable=False, default=0)
+
+    # not a column
+    bookmarks = relationship('Bookmark', backref=__tablename__)
+
+    @staticmethod
+    def get_root():
+        '''
+        Return root folder constants to e.g. create root folder value in DB
+        if it does not exist.
+        '''
+        return (Folder.__ROOT_ID__, Folder.__ROOT_NAME__)
+
+    def __repr__(self):
+        return (
+            "<Folder("
+            "id=%s, name='%s', parent_folder_id=%s"
+            ")>" % (
+                self.id, self.name,
+                self.parent_folder_id,
+            )
+        )
+
+
+class Bookmark(BASE):
+    '''
+    Bookmark item containing URL, title, icon (favicon.ico) and folder_id.
+    '''
+    # pylint: disable=too-few-public-methods
+
+    __tablename__ = 'bookmark'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    title = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    icon = Column(BLOB, nullable=False)
+    folder_id = Column(
+        Integer, ForeignKey('folder.id'),
+        nullable=False, default=0
+    )
+
+    def __repr__(self):
+        return (
+            "<Bookmark("
+            "id=%s, title='%s', url='%s', folder_id=%s"
+            ")>" % (
+                self.id, self.title,
+                self.url, self.folder_id
+            )
+        )
