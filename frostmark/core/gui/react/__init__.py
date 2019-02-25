79db774c039b28fc32cc6d097f26fd61ca622eec
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-02-25 01:34:12 +0100

    [ADD] Add Flask API for listing bookmarks

--- frostmark/core/gui/react/__init__.py
+++ frostmark/core/gui/react/__init__.py
@@ -3,7 +3,9 @@ Module for running a web application wrapping the CLI.
 """
 
 from os.path import join, dirname, abspath
-from flask import Flask
+from flask import Flask, Response
+
+from frostmark.common import fetch_bookmark_tree, json_bookmark_tree
 
 ROOT = dirname(abspath(__file__))
 BUILD = join(ROOT, 'build')
@@ -23,6 +25,20 @@ def index():
 
     with open(join(BUILD, 'index.html'), 'rb') as html:
         return html.read()
+
+
+@APP.route('/list_bookmarks')
+def list_bookmarks():
+    response = Response(
+        response=json_bookmark_tree(fetch_bookmark_tree()),
+        headers={
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': '*'
+        },
+        mimetype='application/json'
+    )
+    return response
 
 
 def main():
