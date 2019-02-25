ea1d788b231784f381fc5aea5ca3c4864a50b6b1
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-02-25 01:16:22 +0100

    [ADD] Add basic React application

---
+++ frostmark/core/gui/react/src/App.js
@@ -0,0 +1,25 @@
+/**
+ * Main application entrypoint assembling all other components.
+ */
+import React, { Component } from 'react';
+import './App.css';
+
+
+/**
+ * Application class included in the index.js/index.html.
+ */
+class App extends Component {
+
+    /**
+     * Render all the components.
+     */
+    render() {
+        return (
+            <div className="App">
+            </div>
+        );
+    }
+}
+
+
+export default App;
