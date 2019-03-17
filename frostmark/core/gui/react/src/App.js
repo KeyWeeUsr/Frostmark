bd8c3f1d442c9fc8036fa573268366c594396b4a
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-03-17 21:30:28 +0100

    [ADD] Include GlobalModal to the App render()

--- frostmark/core/gui/react/src/App.js
+++ frostmark/core/gui/react/src/App.js
@@ -3,6 +3,7 @@
  */
 import React, { Component } from 'react';
 import './App.css';
+import GlobalModal from './GlobalModal';
 import BookmarkList from './BookmarkList';
 
 
@@ -10,16 +11,54 @@ import BookmarkList from './BookmarkList';
  * Application class included in the index.js/index.html.
  */
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            modalIsOpen: false,
+            modalTitle: '',
+            modalBodyBuilder: () => {}
+        };
+
+        // Bind the functions to the instance
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    /**
+     * Create a modal with a custom title and a function that renders its body.
+     */
+    openModal(title, bodyBuilder) {
+        this.setState({
+            modalTitle: title,
+            modalIsOpen: true,
+            modalBodyBuilder: bodyBuilder
+        });
+    }
+
+    /**
+     * Close the modal and clear the defaults.
+     */
+    closeModal() {
+        this.setState({
+            modalTitle: '',
+            modalIsOpen: false,
+            modalBodyBuilder: () => {}
+        });
+    }
 
     /**
      * Render all the components.
      */
     render() {
-        return (
-            <div className="App">
-                <BookmarkList />
-            </div>
-        );
+        return <div className="App">
+            <GlobalModal
+                appRef={this}
+                isOpen={this.state.modalIsOpen}
+                getBody={this.state.modalBodyBuilder}
+                title={this.state.modalTitle}
+            />
+            <BookmarkList appRef={this} />
+        </div>;
     }
 }
 
