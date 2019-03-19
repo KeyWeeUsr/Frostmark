8bfdd0b73383b28ccc4ac6034cac0441bbb3fd3a
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-03-19 22:33:47 +0100

    [ADD] Add Sidebar component with box shadow

---
+++ frostmark/core/gui/react/src/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+
+class Sidebar extends Component {
+    render() {
+        return <div className='Sidebar' />;
+    }
+}
+
+
+export default Sidebar;
