672fb08208a138c513c5861bfa5dd1d30a8feae1
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-03-23 19:44:20 +0100

    [ADD] Add basic menu to the Sidebar

--- frostmark/core/gui/react/src/Sidebar.jsx
+++ frostmark/core/gui/react/src/Sidebar.jsx
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import './Sidebar.css';
 
 import Logo from './Logo';
+import SidebarItem from './SidebarItem';
 
 
 class Sidebar extends Component {
     render() {
         return <div className='Sidebar'>
             <Logo />
+            <SidebarItem text='Import' />
+            <SidebarItem text='Export' />
+            <SidebarItem text='List profiles' />
+            <SidebarItem text='Contribute' />
+            <SidebarItem text='About' />
         </div>;
     }
 }
