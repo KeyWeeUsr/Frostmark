8b8f983fb18352279701089a5ac25fe1fde2eba0
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-05-03 18:40:02 +0200

    [ADD] Add more classifiers to setup.py

--- setup.py
+++ setup.py
@@ -67,12 +67,16 @@ setup(
         'Natural Language :: English',
 
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: JavaScript',
 
         'Topic :: Database :: Front-Ends',
-        'Topic :: Internet'
+        'Topic :: Internet',
+        'Typing :: Typed'
     ],
 
     entry_points={
