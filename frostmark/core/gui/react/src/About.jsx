e05dd5652fbe0936fe6f629b9bac5b6e47fa984b
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-05-03 22:51:49 +0200

    [FIX] Align text in About page properly

--- frostmark/core/gui/react/src/About.jsx
+++ frostmark/core/gui/react/src/About.jsx
@@ -6,20 +6,22 @@ import './About.css';
 class About extends Component {
     render() {
         return <div className='about'>
-<p>Frostmark is a simple bookmarks manager. It can import all bookmarks from
-multiple browsers, list the imported bookmarks and export them via HTML format.
-</p>
+            <p>Frostmark is a simple bookmarks manager. It can import all
+            bookmarks from multiple browsers, list the imported bookmarks and
+            export them via HTML format.</p>
 
-<p>The goal for the future is to be able to extensively manage the bookmarks
-both via console and GUI interface, automatically search the system for the
-available browsers, import the profiles without explicitly specifying paths
-to the browser profiles and export all or only specific folders even directly
-into the browser's bookmarks.</p>
+            <p>The goal for the future is to be able to extensively manage the
+            bookmarks both via console and GUI interface, automatically search
+            the system for the available browsers, import the profiles without
+            explicitly specifying paths to the browser profiles and export all
+            or only specific folders even directly into the browser&#39;s
+            bookmarks.</p>
 
-<p>Feel free to open a pull request with your improvements for the project,
-open an issue in case of bug or a feature request and come and talk about the
-project in the <a href="https://riot.im/app/#/group/+frostmark:matrix.org">
-Matrix community</a></p>
+            <p>Feel free to open a pull request with your improvements for the
+            project, open an issue in case of bug or a feature request and come
+            and talk about the project in the &nbsp;
+            <a href="https://riot.im/app/#/group/+frostmark:matrix.org">
+            Matrix community</a></p>
         </div>;
     }
 }
